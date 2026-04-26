2e775384627d274ec583b75e6d28fe5ede08f8ba
Ember Dart <git@jolharg.com>
2026-04-26 16:10:05 +0100

    update shell.nix

--- haskell/tumblr-editor/shell.nix
+++ haskell/tumblr-editor/shell.nix
@@ -1,12 +1,12 @@
 with import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.zip") {};
 # needs mkShell in order to use headers/etc. from deps! how do we do that from nix-shell
 let cabalPkgs = [
-    { name = "apply-refact"; exe = "refactor"; ver = "0.15.0.0"; enable = false; }
-    { name = "cabal-add"; ver = "0.2"; enable = false; }
+    # { name = "apply-refact"; exe = "refactor"; ver = "0.15.0.0"; }
+    { name = "cabal-add"; ver = "0.2"; }
     { name = "cabal-fmt"; ver = "0.1.12"; }
-    { name = "doctest"; ver = "0.24.3"; flags = "-fcabal-doctest"; }
+    { name = "doctest"; exe = "cabal-doctest"; ver = "0.24.3"; flags = "-fcabal-doctest"; }
     { name = "ghcid"; ver = "0.8.9"; }
-    { name = "ghci-dap"; ver = "0.0.26.0"; enable = false; }
+    # { name = "ghci-dap"; ver = "0.0.26.0"; }
     { name = "krank"; ver = "0.3.1"; flags = "--allow-newer=template-haskell"; }
     { name = "haskell-debug-adapter"; ver = "0.0.42.0"; flags = "--allow-newer=aeson,bytestring,containers,deepseq,template-haskell,time"; }
     { name = "haskell-language-server"; ver = "2.13.0.0"; flags = "--allow-newer=containers,deepseq,ghc,hiedb,template-haskell,time,websockets"; }
@@ -15,9 +15,8 @@ let cabalPkgs = [
     { name = "hoogle"; ver = "5.0.19.0"; flags = "--allow-newer=template-haskell"; }
     { name = "hpack"; ver = "0.39.3"; flags = "--allow-newer=template-haskell"; }
     { name = "implicit-hie"; exe = "gen-hie"; ver = "0.1.4.0"; flags = "--allow-newer=bytestring,template-haskell"; }
-    { name = "stan"; ver = "0.2.1.0"; enable = false; }
+    # { name = "stan"; ver = "0.2.1.0"; }
     { name = "stylish-haskell"; ver = "0.15.1.0"; flags = "--allow-newer=template-haskell"; }
-    { name = "weeder"; ver = "2.10.0"; enable = false; }
 ]; in
 mkShell rec {
     packages = [
@@ -29,13 +28,13 @@ mkShell rec {
         pcre.dev
     ];
     shellHook = ''
-        # cabal update >/dev/null
+        cabal update >/dev/null
         set -euo pipefail
         ${lib.concatStringsSep "\n" (builtins.map ({name, exe ? name, ver, flags ? ""}:
-            "[[ -f ~/.local/bin/${exe} ]] || cabal install ${name}-${ver} ${flags} --overwrite-policy=always 2>&1 | sed 's/^/${name}: /g' 2>&1"
-        ) (builtins.filter ({ enable ? true, ... }: enable) cabalPkgs))}        
+            "[[ -f ~/.local/bin/${exe} ]] || echo cabal install ${name}-${ver} ${flags} >&2"
+        ) cabalPkgs)}        
         export PATH="~/.local/bin:$PATH"
         # gen-hie > hie.yaml
-        # for i in $(find . -type f | grep -v "dist-*"); do krank $i 2>&1; done
+        # for i in $(find . -type f | grep -v "dist-*"); do krank $i || : >&2; done
     '';
 }
